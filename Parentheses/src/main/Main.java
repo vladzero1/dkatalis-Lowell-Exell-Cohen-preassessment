c6b74e22c5004c08571785ca4a62c7ac34825e9c
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2020-03-29 12:02:04 +0700

    Parentheses

--- Parentheses/src/main/Main.java
+++ Parentheses/src/main/Main.java
@@ -1,56 +1,50 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import sun.security.util.Length;
+
 public class Main {
 	private static final Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-        String n = scanner.nextLine();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String n = scanner.nextLine();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        String result = Validate(n);
-        
-        System.out.print(String.valueOf(result));
-        scanner.close();
-    }
-	
-	public static String Validate(String data)
-	{
-		//original regex
-//		Pattern pattern = Pattern.compile("([a-zA-Z0-9_]+)([@])([a-zA-Z0-9_]+)([.])(.+)");
-		//cannot access group
-		Pattern[] arr = new Pattern[3];
-		Pattern[] arr1 = new Pattern[3];
-		
-		Pattern pattern = Pattern.compile("[[]");
-		arr[0] = pattern;
-		Pattern pattern1 = Pattern.compile("[{]");
-		arr[1] = pattern;
-		Pattern pattern2 = Pattern.compile("[(]");
-		arr[2] = pattern;
-		
-		Pattern pattern3 = Pattern.compile("[]]");
-		arr1[0] = pattern;
-		Pattern pattern4 = Pattern.compile("[}]");
-		arr1[1] = pattern;
-		Pattern pattern5 = Pattern.compile("[)]");
-		arr1[2] = pattern;
-		for(Pattern ptr : arr)
-		{
-			Matcher matcher = ptr.matcher(data);
-			if(matcher.find())
-			{
-				data = data.substring(matcher.group(1).length());
-				break;
-			}
+		boolean result = Validate(n);
+
+		System.out.print(String.valueOf(result));
+		scanner.close();
+	}
+
+	public static boolean Validate(String data) {
+		List<String> arr = new ArrayList<String>();
+		HashMap<String, String> map = new HashMap<>();
+		map.put("[", "]");
+		map.put("{", "}");
+		map.put("(", ")");
+		Pattern pattern1 = Pattern.compile("([\\[\\]{}\\(\\)])(.+)*?");
+
+		Matcher matcher = pattern1.matcher(data);
+		while (matcher.find()) {
+			arr.add(matcher.group(1));
 		}
-		return data;
-		
-		
 
-		
-		
+		if (arr.size() % 2 == 0) {
+			for (int i = 0; i < arr.size()/2; i++) {
+				 if(!map.get(arr.get(i)).equalsIgnoreCase(arr.get(arr.size()-i-1)))
+				 {
+					 return false;
+				 }
+			}
+			return true;
+		} else
+			return false;
+
 	}
 }
