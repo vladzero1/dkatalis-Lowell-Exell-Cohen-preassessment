af453d45f9a63a63c7e67e3cf10eccc5a10187f6
vladzero1 <37445555+vladzero1@users.noreply.github.com>
2020-04-02 11:57:16 +0700

    Validating Number
    Complete

--- Validating Number/src/main/Main.java
+++ Validating Number/src/main/Main.java
@@ -1,5 +1,32 @@
 package main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public final static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		String str = sc.nextLine().replaceAll(" ", "");
+		char[] c = str.toCharArray();
+		int sum = 0;
+		for(int i = 0;i<c.length;i++)
+		{
+			if(i%2 == 0)
+			{
+				int num = Character.getNumericValue(c[i])*2;
+				if(num > 9) 
+					num %= 9;
+				sum += num;
+			}
+			else
+			{
+				int num = Character.getNumericValue(c[i]);
+				sum += num;
+			}
+		}
+		if(sum%10 == 0)
+			System.out.println("The Number is Valid");
+		else
+			System.out.println("The Number is Invalid");
+	}
 }
